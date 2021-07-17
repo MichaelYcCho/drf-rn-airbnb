c870338026b7f4e134210537ab6d90f60d734e4e
Youngchan <michael_cho77@naver.com>
2021-07-18 00:46:44 +0900

    feature/FavsView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MeView, user_detail
+from .views import MeView, user_detail, FavsView
 
 app_name = "users"
 
 urlpatterns = [
     path("me/", MeView.as_view()),
+    path("me/fav/", FavsView.as_view()),
     path("<int:pk>/", user_detail),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,9 +3,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
+
 from .models import User
 from .serializers import ReadUserSerializer, WriteUserSerializer
-
+from rooms.models import Room
+from rooms.serializers import RoomSerializer
 
 class MeView(APIView):
 
@@ -30,3 +32,28 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    def put(self, request):
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
