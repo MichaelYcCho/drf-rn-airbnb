4b1784cb181a148e45b81d1d45d709c468f9b05d
Youngchan <michael_cho77@naver.com>
2021-07-18 23:42:25 +0900

    feature/ SerializerMethodField

--- backend/rooms/serializers.py
+++ backend/rooms/serializers.py
@@ -5,6 +5,8 @@ from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    is_fav = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
         exclude = ("modified", )
@@ -21,3 +23,10 @@ class RoomSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Not enough time between changes")
         return data
 
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
