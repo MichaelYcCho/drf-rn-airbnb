b5b21a088e3328525caf3cdbc5cc92b920daac84
Youngchan <michael_cho77@naver.com>
2021-07-18 23:06:58 +0900

    feature/Pagination

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RoomsView, RoomView
+from .views import RoomsView, RoomView, room_search
 
 
 app_name = "rooms"
 
 urlpatterns = [
     path('list/', RoomsView.as_view()),
+    path("search/", room_search),
     path("<int:pk>/", RoomView.as_view()),
 ]
