832f62544a487e179fd55d991eca61a7d03f36d3
Youngchan <michael_cho77@naver.com>
2021-07-20 20:35:47 +0900

    feature/Favs ViewSet

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,18 +3,17 @@ from django.conf import settings
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
-
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsSelf
+
 
 class UsersViewSet(ModelViewSet):
 
@@ -22,12 +21,17 @@ class UsersViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
+
         if self.action == "list":
             permission_classes = [IsAdminUser]
-        elif self.action == "create" or self.action == "retrieve":
+        elif (
+            self.action == "create"
+            or self.action == "retrieve"
+            or self.action == "favs"
+        ):
             permission_classes = [AllowAny]
         else:
-            permission_classes = [IsSelf | IsAdminUser]
+            permission_classes = [IsSelf]
         return [permission() for permission in permission_classes]
 
     @action(detail=False, methods=["post"])
@@ -45,19 +49,16 @@ class UsersViewSet(ModelViewSet):
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-
-class FavsView(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
+    @action(detail=True)
+    def favs(self, request, pk):
+        user = self.get_object()
         serializer = RoomSerializer(user.favs.all(), many=True).data
         return Response(serializer)
 
-    def put(self, request):
+    @favs.mapping.put
+    def toggle_favs(self, request, pk):
         pk = request.data.get("pk", None)
-        user = request.user
+        user = self.get_object()
         if pk is not None:
             try:
                 room = Room.objects.get(pk=pk)
