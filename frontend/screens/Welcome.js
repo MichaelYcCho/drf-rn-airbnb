df4341c4b208f0eb24c14f47450f8507dbb70bba
Youngchan <michael_cho77@naver.com>
2021-07-23 21:16:40 +0900

    feature/Auth Navigation

---
+++ frontend/screens/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+export default ({ navigation }) => {
+    return (
+        <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+            <Text>Welcome</Text>
+            <Button onPress={() => navigation.navigate("SignUp")} title={"Sign Up"} />
+            <Button onPress={() => navigation.navigate("SignIn")} title={"Sign In"} />
+        </View>
+    );
+};
