4d602c1891604073d277adc6d69c908706ad8b44
Youngchan <michael_cho77@naver.com>
2021-07-26 23:41:36 +0900

    feature/Fetching Rooms

--- frontend/api.js
+++ frontend/api.js
@@ -18,6 +18,6 @@ const callApi = async (method, path, data, jwt) => {
 export default {
     createAccount: form => callApi("post", "/users/", form),
     login: form => callApi("post", "/users/login/", form),
-    rooms: page => callApi("get", `/rooms/?page=${page}`)
+    rooms: (page = 1) => callApi("get", `/rooms/?page=${page}`)
 };
 
--- frontend/redux/roomsSlice.js
+++ frontend/redux/roomsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import api from "../api";
 
 const roomsSlice = createSlice({
     name: "rooms",
@@ -10,13 +11,36 @@ const roomsSlice = createSlice({
         favs: []
     },
     reducers: {
-        setExploreRooms: (state, action) => {
-            state.explore.rooms.push(action.payload.rooms);
-            state.explore.page = action.payload.page
+        setExploreRooms(state, action) {
+            const { explore } = state;
+            const { payload } = action;
+            payload.rooms.forEach(payloadRoom => {
+                const exists = explore.rooms.find(
+                    savedRoom => savedRoom.id === payloadRoom.id
+                );
+                if (!exists) {
+                    explore.rooms.push(payloadRoom);
+                }
+            });
+            state.explore.page = payload.page;
         }
     }
 });
 
 const { setExploreRooms } = roomsSlice.actions;
+
+export const getRooms = () => async dispatch => {
+    try {
+        const {
+            data: { results }
+        } = await api.rooms();
+        dispatch(
+            setExploreRooms({
+                rooms: results,
+                page: 1
+            })
+        );
+    } catch (e) { }
+};
 
 export default roomsSlice.reducer;
