8117c1104c7f0cba8ee48178583807fb154831a5
Youngchan <michael_cho77@naver.com>
2021-07-29 00:41:54 +0900

    feature/Toggle Favs

--- frontend/api.js
+++ frontend/api.js
@@ -3,7 +3,7 @@ import { api } from "./api_address";
 
 const callApi = async (method, path, data, jwt) => {
     const headers = {
-        Authorization: jwt,
+        Authorization: `Bearer ${jwt}`,
         "Content-Type": "application/json"
     };
     const baseUrl = `${api}/api/v1`;
@@ -19,6 +19,7 @@ export default {
     createAccount: form => callApi("post", "/users/", form),
     login: form => callApi("post", "/users/login/", form),
     rooms: (page = 1) => callApi("get", `/rooms/?page=${page}`),
-    favs: id => callApi("get", `/users/${id}/favs/`)
+    favs: id => callApi("get", `/users/${id}/favs/`),
+    toggleFavs: (userId, roomId, token) =>
+        callApi("put", `/users/${userId}/favs/`, { pk: roomId }, token)
 };
-
---
+++ frontend/screens/Main/Saved/SavedPresenter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  margin-top: 80px;
+  padding: 0px 30px;
+`;
+
+const SV = styled.ScrollView``;
+
+const Title = styled.Text`
+  font-size: 36px;
+`;
+
+export default () => (
+    <Container>
+        <Title>Favourites</Title>
+        <SV></SV>
+    </Container>
+);
