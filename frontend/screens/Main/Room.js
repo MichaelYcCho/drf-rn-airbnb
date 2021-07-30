2238c26806ad2d543d152f40e0cc9a85fed05980
Youngchan <michael_cho77@naver.com>
2021-07-31 00:10:48 +0900

    feature/Styling Room Detail

--- frontend/screens/Main/Room.js
+++ frontend/screens/Main/Room.js
@@ -1,10 +1,68 @@
 import React, { useEffect } from "react";
 import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
 import RoomPhotos from "../../components/RoomPhotos";
+import colors from "../../colors";
+import utils from "../../utils";
 
 const Container = styled.ScrollView``;
 
-const Text = styled.Text``;
+const DataContainer = styled.View`
+  padding: 0px 20px;
+`;
+
+const Address = styled.Text`
+  margin-top: 10px;
+  font-size: 24px;
+`;
+
+const PropertyInfoContainer = styled.View`
+  margin-top: 20px;
+  flex-direction: row;
+`;
+
+const PropertyInfoData = styled.View`
+  background-color: ${colors.green};
+  margin-right: 10px;
+  border-radius: 5px;
+`;
+
+const PropertyInfoText = styled.Text`
+  color: white;
+  font-weight: 500;
+  padding: 5px 10px;
+`;
+
+const CheckContainer = styled.View`
+  margin-top: 40px;
+`;
+
+const CheckTitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const CheckTitle = styled.Text`
+  font-size: 18px;
+  margin-left: 15px;
+`;
+
+const CheckTime = styled.Text`
+  margin-top: 10px;
+`;
+
+function formatQtt(number, name) {
+    if (number === 1) {
+        return `${number} ${name}`;
+    } else {
+        return `${number} ${name}s`;
+    }
+}
+
+function formatTime(time) {
+    const [hours, min, sec] = time.split(":");
+    return `${hours} o'clock.`;
+}
 
 export default ({ route: { params }, navigation }) => {
     useEffect(() => {
@@ -12,7 +70,37 @@ export default ({ route: { params }, navigation }) => {
     }, []);
     return (
         <Container>
-            <RoomPhotos photos={params.photos} />
+            <RoomPhotos photos={params.photos} factor={2} />
+            <DataContainer>
+                <Address>{params.address}</Address>
+                <PropertyInfoContainer>
+                    <PropertyInfoData>
+                        <PropertyInfoText>{formatQtt(params.beds, "bed")}</PropertyInfoText>
+                    </PropertyInfoData>
+                    <PropertyInfoData>
+                        <PropertyInfoText>
+                            {formatQtt(params.bedrooms, "bedroom")}
+                        </PropertyInfoText>
+                    </PropertyInfoData>
+                    <PropertyInfoData>
+                        <PropertyInfoText>
+                            {formatQtt(params.bathrooms, "bathroom")}
+                        </PropertyInfoText>
+                    </PropertyInfoData>
+                </PropertyInfoContainer>
+                <CheckContainer>
+                    <CheckTitleContainer>
+                        <Ionicons
+                            name={utils.isAndroid() ? "md-timer" : "ios-timer"}
+                            size={24}
+                        />
+                        <CheckTitle>Check-in / Check-out</CheckTitle>
+                    </CheckTitleContainer>
+                    <CheckTime>
+                        {formatTime(params.check_in)} / {formatTime(params.check_out)}
+                    </CheckTime>
+                </CheckContainer>
+            </DataContainer>
         </Container>
     );
 };
