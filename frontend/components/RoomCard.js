be5cc91be69cfd0ba5ea4c77f8ad596d5f443011
Youngchan <michael_cho77@naver.com>
2021-07-30 23:48:42 +0900

    feature/Room Detail

--- frontend/components/RoomCard.js
+++ frontend/components/RoomCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Pt from "prop-types";
 import styled from "styled-components/native";
-import { Dimensions, View } from "react-native";
+import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import Swiper from "react-native-web-swiper";
 import utils from "../utils";
 import { useDispatch } from "react-redux";
 import { toggleFav } from "../redux/usersSlice";
 import colors from "../colors";
+import { useNavigation } from "@react-navigation/native";
+import RoomPhotos from "./RoomPhotos";
 
-const { width, height } = Dimensions.get("screen");
 
 const Container = styled.View`
   width: 100%;
@@ -50,19 +50,6 @@ const PriceNumber = styled.Text`
   font-size: 16px;
 `;
 
-const PhotosContainer = styled.View`
-  margin-bottom: 10px;
-  overflow: hidden;
-  width: 100%;
-  height: ${height / 4}px;
-  border-radius: 4px;
-`;
-
-const SlideImage = styled.Image`
-  width: 100%;
-  height: 100%;
-`;
-
 const FavButton = styled.View`
   background-color: white;
   width: 50px;
@@ -94,8 +81,9 @@ function getIconName(isFav) {
   }
 }
 
-const RoomCard = ({ id, isFav, isSuperHost, photos, name, price }) => {
+const RoomCard = ({ id, isFav, isSuperHost, photos, name, price, roomObj }) => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
   return (
     <Container>
       <TOpacity onPress={() => dispatch(toggleFav(id))}>
@@ -107,40 +95,22 @@ const RoomCard = ({ id, isFav, isSuperHost, photos, name, price }) => {
           />
         </FavButton>
       </TOpacity>
-      <PhotosContainer>
-        {photos.length === 0 ? (
-          <SlideImage
-            resizeMode="repeat"
-            source={require("../assets/roomDefault.jpg")}
-          />
-        ) : (
-          <Swiper
-            controlsProps={{
-              PrevComponent: () => null,
-              NextComponent: () => null,
-              dotActiveStyle: {
-                backgroundColor: "white"
-              }
-            }}
-          >
-            {photos.map(photo => (
-              <View key={photo.id}>
-                <SlideImage source={{ uri: photo.file }} />
-              </View>
-            ))}
-          </Swiper>
-        )}
-      </PhotosContainer>
-      {isSuperHost ? (
-        <Superhost>
-          <SuperhostText>Superhost</SuperhostText>
-        </Superhost>
-      ) : null}
-      <Name>{name}</Name>
-      <PriceContainer>
-        <PriceNumber>${price}</PriceNumber>
-        <PriceText> / night</PriceText>
-      </PriceContainer>
+      <RoomPhotos photos={photos} />
+      <TouchableOpacity
+        style={{ alignItems: "flex-start" }}
+        onPress={() => navigation.navigate("RoomDetail", { ...roomObj })}
+      >
+        {isSuperHost ? (
+          <Superhost>
+            <SuperhostText>Superhost</SuperhostText>
+          </Superhost>
+        ) : null}
+        <Name>{name}</Name>
+        <PriceContainer>
+          <PriceNumber>${price}</PriceNumber>
+          <PriceText> / night</PriceText>
+        </PriceContainer>
+      </TouchableOpacity>
     </Container>
   );
 };
@@ -155,7 +125,8 @@ RoomCard.propTypes = {
     })
   ),
   name: Pt.string.isRequired,
-  price: Pt.number.isRequired
+  price: Pt.number.isRequired,
+  roomObj: Pt.object.isRequired
 };
 
 export default RoomCard;
---
+++ frontend/screens/Main/Room.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import styled from "styled-components/native";
+import RoomPhotos from "../../components/RoomPhotos";
+
+const Container = styled.ScrollView``;
+
+const Text = styled.Text``;
+
+export default ({ route: { params }, navigation }) => {
+    useEffect(() => {
+        navigation.setOptions({ title: params.name });
+    }, []);
+    return (
+        <Container>
+            <RoomPhotos photos={params.photos} />
+        </Container>
+    );
+};
