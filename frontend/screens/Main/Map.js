217485bb2c68d997616e90a59f99b9c0600b47d8
Youngchan <michael_cho77@naver.com>
2021-08-01 21:50:12 +0900

    feature/Map Screen

--- frontend/screens/Main/Map.js
+++ frontend/screens/Main/Map.js
@@ -1,16 +1,104 @@
 import React from "react";
 import styled from "styled-components/native";
+import { StyleSheet, Dimensions } from "react-native";
+import { connect } from "react-redux";
+import MapView from "react-native-maps";
+
+const { width, height } = Dimensions.get("screen");
 
 const Container = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
-  flex: 1;
 `;
 
-const Text = styled.Text``;
+const ScrollView = styled.ScrollView`
+  position: absolute;
+  bottom: 50px;
+`;
 
-export default () => (
-  <Container>
-    <Text>Map</Text>
-  </Container>
-);
+const RoomContainer = styled.View`
+  background-color: transparent;
+  width: ${width}px;
+  align-items: center;
+`;
+
+const RoomCard = styled.View`
+  background-color: white;
+  width: ${width - 80}px;
+  height: 120px;
+  margin-right: 20px;
+  border-radius: 10px;
+  padding: 0px 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const RoomPhoto = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 5px;
+  margin-right: 20px;
+`;
+
+const Column = styled.View`
+  width: 70%;
+`;
+
+const RoomName = styled.Text`
+  font-size: 18px;
+`;
+
+const RoomPrice = styled.Text`
+  margin-top: 5px;
+  font-size: 16px;
+`;
+
+const Map = ({ rooms }) => {
+  return (
+    <Container>
+      <MapView
+        style={StyleSheet.absoluteFill}
+        camera={{
+          center: {
+            latitude: parseFloat(rooms[0].lat),
+            longitude: parseFloat(rooms[0].lng)
+          },
+          altitude: 700,
+          pitch: 0,
+          heading: 0,
+          zoom: 10
+        }}
+      />
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        horizontal
+      >
+        {rooms?.map(room => (
+          <RoomContainer key={room.id}>
+            <RoomCard>
+              <RoomPhoto
+                source={
+                  room.photos[0]?.file
+                    ? { uri: room.photos[0]?.file }
+                    : require("../../assets/roomDefault.jpg")
+                }
+              />
+              <Column>
+                <RoomName>{room.name}</RoomName>
+                <RoomPrice>${room.price}</RoomPrice>
+              </Column>
+            </RoomCard>
+          </RoomContainer>
+        ))}
+      </ScrollView>
+    </Container>
+  );
+};
+
+function mapStateToProps(state) {
+  return { rooms: state.roomsReducer.explore.rooms };
+}
+
+export default connect(mapStateToProps)(Map);
